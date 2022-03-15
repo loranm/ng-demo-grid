f58ce5e5b6f8b23c3f2be9ee35564d8f54eddbd7
loran <laurent.muller@wizbii.com>
2022-03-15 21:09:13 +0100

    STEPS: Added done toggle

--- src/app/features/center-stuffs/center-stuffs/center-stuffs/center-stuffs.component.ts
+++ src/app/features/center-stuffs/center-stuffs/center-stuffs/center-stuffs.component.ts
@@ -1,11 +1,20 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-center-stuffs',
   templateUrl: './center-stuffs.component.html',
-  styleUrls: ['./center-stuffs.component.scss'],
+  styleUrls: [
+    './center-stuffs.component.scss',
+    './center-stuffs-done.component.scss',
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CenterStuffsComponent {
+  isDone$ = new BehaviorSubject(false);
   readonly bee = 'assets/bee.svg';
+
+  toggleChange(): void {
+    this.isDone$.next(!this.isDone$.value);
+  }
 }
--- src/app/features/sizing-blocks/sizing-blocks.module.ts
+++ src/app/features/sizing-blocks/sizing-blocks.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SizingBlocksComponent } from './sizing-blocks/sizing-blocks/sizing-blocks.component';
 import { SizingBlocksRoutingModule } from './sizing-blocks-routing.module';
 import { BlockModule } from '@commons/block/block.component';
+import { SeparatorModule } from '@commons/separator/separator.component';
 
 @NgModule({
   declarations: [SizingBlocksComponent],
-  imports: [CommonModule, SizingBlocksRoutingModule, BlockModule],
+  imports: [
+    CommonModule,
+    SizingBlocksRoutingModule,
+    BlockModule,
+    SeparatorModule,
+  ],
 })
 export class SizingBlocksModule {}
