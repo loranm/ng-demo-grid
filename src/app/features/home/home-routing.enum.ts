728cd2331c31ecd1924cf8578c2da9998fc1d22e
loran <laurent.muller@wizbii.com>
2022-03-19 06:59:20 +0100

    STEPS: Removed useless code

--- src/app/features/home/home-routing.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum HomeRoutingEnum {
-  Step1 = 'step1',
-  Step2 = 'step2',
-  Final = 'final',
-}
--- src/app/features/home/home/steps.ts
+++
@@ -1,7 +0,0 @@
-import { HomeRoutingEnum } from '../home-routing.enum';
-
-export const STEPS: readonly HomeRoutingEnum[] = [
-  HomeRoutingEnum.Step1,
-  HomeRoutingEnum.Step2,
-  HomeRoutingEnum.Final,
-] as const;
